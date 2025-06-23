0cbd9a43882dad68f00818c3788d4e9c63d5d3f7
nanaelie <elienana92@gmail.com>
2025-06-23 04:56:54 +0000

    docs: update

---
+++ api/extract.ts
@@ -0,0 +1,30 @@
+// api/extract.ts
+import extract from '../extract';
+
+module.exports = (req, res) => {
+	if (req.method !== 'GET') {
+		res.status(405).send('Method Not Allowed');
+		return;
+	}
+
+	try {
+		const url = req.query.url as string;
+
+		res.setHeader('Content-Type', 'application/json');
+		if (url.trim() === '') {
+			res.status(200).send({
+				error: true,
+				message: "L'url est vide!",
+			});
+		} else {
+			const emails: string[] = await extract(url.trim());
+			res.status(200).send({
+				error: false,
+				message: emails,
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Erreur serveur.");
+	}
+};
